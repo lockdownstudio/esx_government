e1c8a799c34bdd4d789f42802df6ab645b3d5a6b
Xrez31 <66789826+Xrez31@users.noreply.github.com>
2020-06-26 07:38:27 +0800

    initial commit

---
+++ locales/en.lua
@@ -0,0 +1,95 @@
+Locales['en'] = {
+	-- Cloakroom
+	['cloakroom'] = 'Cloakroom',
+	['citizen_wear'] = 'Civilian attire',
+	['government_outfit'] = 'Realtor',
+	['government_outfit_1'] = 'Law',
+	['government_outfit_2'] = 'Judge',
+	['government_outfit_3'] = 'Secretary',
+	['government_outfit_4'] = 'Mayor',
+	['government_outfit_5'] = 'Governor',    
+    	['no_outfit'] = 'There is no uniform for your size ...',
+    	['open_cloackroom'] = 'Press ~INPUT_CONTEXT~ to change yourself',
+	
+	-- Vehicles  
+    	['vehicle_menu'] = 'Vehicle',
+    	['vehicle_out'] = 'There is already a vehicle outside',
+    	['vehicle_spawner'] = 'Press ~INPUT_CONTEXT~ to exit a vehicle',
+    	['store_vehicle'] = 'Press ~INPUT_CONTEXT~ to store the vehicle',
+    	['service_max'] = 'Full service:',
+		['spawn_point_busy'] = 'A vehicle occupies the exit point',
+		['helicopter_prompt'] = 'Helicopter',
+	
+	-- Boss Menu
+	['deposit_society'] = 'deposit money',
+	['withdraw_society'] = 'withdraw company money',
+	['boss_actions'] = 'Boss action',
+	-- Misc
+	['invalid_amount'] = '~r~ invalid amount',
+	['open_menu'] = 'press ~INPUT_CONTEXT~ to open the menu',
+	['deposit_amount'] = 'deposit amount',
+	['money_withdraw'] = 'withdrawal amount',
+ 
+    	-- Vault  
+    	['get_weapon'] = 'Take Weapon',
+    	['put_weapon'] = 'Drop Weapon',
+    	['get_weapon_menu'] = 'Chest - Take Weapon',
+    	['put_weapon_menu'] = 'Safe - Deposit Weapon',
+    	['get_object'] = 'Take Object',
+    	['put_object'] = 'Drop Object',
+    	['vault'] = 'Safe',
+    	['open_vault'] = 'Press ~INPUT_CONTEXT~ to access the safe',
+
+    	-- Boss Menu  
+    	['take_company_money'] = 'Withdraw company money',
+    	['deposit_money'] = 'Deposit money',
+    	['amount_of_withdrawal'] = 'Withdrawal amount',
+    	['invalid_amount'] = 'Invalid amount',
+    	['amount_of_deposit'] = 'Deposit amount',
+    	['open_bossmenu'] = 'Press ~INPUT_CONTEXT~ to open the menu',
+    	['invalid_quantity'] = 'Invalid quantity',
+    	['you_removed'] = 'You have removed x',
+    	['you_added'] = 'You added x',
+    	['quantity'] = 'Quantity',
+    	['inventory'] = 'Inventory',
+		['government_stock'] = 'Stock',
+		['have_withdrawn'] = 'you have withdrawn ~y~%sx~s~ ~b~%s~s~',
+
+    	-- Misc 
+    	['map_blip'] = 'Lockdown News',
+
+    	-- Billing Menu  
+    	['billing'] = 'Invoice',
+    	['no_players_nearby'] = 'No player nearby',
+    	['billing_amount'] = 'Bill amount',
+		['amount_invalid'] = 'Invalid amount',
+		
+		--- Vehicles
+		['vehicle_blocked'] = 'all available spawn points are currently blocked!',
+		['garage_title'] = 'vehicle Actions',
+		['garage_stored'] = 'stored',
+		['garage_notstored'] = 'not in garage',
+		['garage_storing'] = 'we\'re attempting to remove the vehicle, make sure no players are around it.',
+		['garage_has_stored'] = 'the vehicle has been stored in your garage',
+		['garage_has_notstored'] = 'no nearby owned vehicles were found',
+		['garage_notavailable'] = 'your vehicle is not stored in the garage.',
+		['garage_blocked'] = 'there\'s no available spawn points!',
+		['garage_empty'] = 'you dont have any vehicles in your garage.',
+		['garage_released'] = 'your vehicle has been released from the garage.',
+		['garage_store_nearby'] = 'there is no nearby vehicles.',
+		['garage_storeditem'] = 'open garage',
+		['garage_storeitem'] = 'store vehicle in garage',
+		['garage_buyitem'] = 'vehicle shop',
+		['garage_notauthorized'] = 'you\'re not authorized to buy this kind of vehicles.',
+		['shop_item'] = '$%s',
+		['vehicleshop_title'] = 'vehicle Shop',
+		['vehicleshop_confirm'] = 'do you want to buy this vehicle?',
+		['vehicleshop_bought'] = 'you have bought ~y~%s~s~ for ~r~$%s~s~',
+		['vehicleshop_money'] = 'you cannot afford that vehicle',
+		['vehicleshop_awaiting_model'] = 'the vehicle is currently ~g~DOWNLOADING & LOADING~s~ please wait',
+		['confirm_no'] = 'no',
+		['confirm_yes'] = 'yes',
+		['gov_actions'] = 'Government Actions',
+		['no_players_nearby'] = 'There is no player nearby!',
+		['comserve'] = 'Community Service',
+}
---
+++ server/main.lua
@@ -0,0 +1,237 @@
+ESX                = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+if Config.MaxInService ~= -1 then
+  TriggerEvent('esx_service:activateService', 'government', Config.MaxInService)
+end
+
+TriggerEvent('esx_society:registerSociety', 'government', 'government', 'society_government', 'society_government', 'society_government', {type = 'private'})
+
+RegisterNetEvent('esx_government:getStockItem')
+AddEventHandler('esx_government:getStockItem', function(itemName, count)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_government', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- is there enough in the society?
+		if count > 0 and inventoryItem.count >= count then
+
+			-- can the player carry the said amount of x item?
+			if xPlayer.canCarryItem(itemName, count) then
+				inventory.removeItem(itemName, count)
+				xPlayer.addInventoryItem(itemName, count)
+				xPlayer.showNotification(_U('have_withdrawn', count, inventoryItem.label))
+			else
+				xPlayer.showNotification(_U('invalid_quantity'))
+			end
+		else
+			xPlayer.showNotification(_U('invalid_quantity'))
+		end
+	end)
+end)
+
+RegisterNetEvent('esx_government:putStockItems')
+AddEventHandler('esx_government:putStockItems', function(itemName, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_government', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- does the player have enough of the item?
+		if sourceItem.count >= count and count > 0 then
+			xPlayer.removeInventoryItem(itemName, count)
+			inventory.addItem(itemName, count)
+			xPlayer.showNotification(_U('have_deposited', count, inventoryItem.label))
+		else
+			xPlayer.showNotification(_U('invalid_quantity'))
+		end
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_government:getStockItems', function(source, cb)
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_government', function(inventory)
+		cb(inventory.items)
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_government:getPlayerInventory', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local items   = xPlayer.inventory
+
+	cb({items = items})
+end)
+
+ESX.RegisterServerCallback('esx_government:getFineList', function(source, cb, category)
+
+  MySQL.Async.fetchAll(
+    'SELECT * FROM fine_types_government WHERE category = @category',
+    {
+      ['@category'] = category
+    },
+    function(fines)
+      cb(fines)
+    end
+  )
+
+end)
+
+ESX.RegisterServerCallback('esx_government:getVaultWeapons', function(source, cb)
+
+  TriggerEvent('esx_datastore:getSharedDataStore', 'society_government', function(store)
+
+    local weapons = store.get('weapons')
+
+    if weapons == nil then
+      weapons = {}
+    end
+
+    cb(weapons)
+
+  end)
+
+end)
+
+ESX.RegisterServerCallback('esx_government:addVaultWeapon', function(source, cb, weaponName, removeWeapon)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if removeWeapon then
+		xPlayer.removeWeapon(weaponName)
+	end
+
+	TriggerEvent('esx_datastore:getSharedDataStore', 'society_government', function(store)
+		local weapons = store.get('weapons') or {}
+		local foundWeapon = false
+
+		for i=1, #weapons, 1 do
+			if weapons[i].name == weaponName then
+				weapons[i].count = weapons[i].count + 1
+				foundWeapon = true
+				break
+			end
+		end
+
+		if not foundWeapon then
+			table.insert(weapons, {
+				name  = weaponName,
+				count = 1
+			})
+		end
+
+		store.set('weapons', weapons)
+		cb()
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_government:removeVaultWeapon', function(source, cb, weaponName)
+
+  local xPlayer = ESX.GetPlayerFromId(source)
+
+  xPlayer.addWeapon(weaponName, 1000)
+
+  TriggerEvent('esx_datastore:getSharedDataStore', 'society_government', function(store)
+
+    local weapons = store.get('weapons') or {}
+
+    local foundWeapon = false
+
+    for i=1, #weapons, 1 do
+      if weapons[i].name == weaponName then
+        weapons[i].count = (weapons[i].count > 0 and weapons[i].count - 1 or 0)
+		foundWeapon = true
+		break
+      end
+    end
+
+    if not foundWeapon then
+      table.insert(weapons, {
+        name  = weaponName,
+        count = 0
+      })
+    end
+
+     store.set('weapons', weapons)
+
+     cb()
+
+  end)
+
+end)
+
+ESX.RegisterServerCallback('esx_government:buyJobVehicle', function(source, cb, vehicleProps, type)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local price = getPriceFromHash(vehicleProps.model, xPlayer.job.grade_name, type)
+
+	-- vehicle model not found
+	if price == 0 then
+		print(('esx_government: %s attempted to exploit the shop! (invalid vehicle model)'):format(xPlayer.identifier))
+		cb(false)
+	else
+		if xPlayer.getMoney() >= price then
+			xPlayer.removeMoney(price)
+
+			MySQL.Async.execute('INSERT INTO owned_vehicles (owner, vehicle, plate, type, job, `stored`) VALUES (@owner, @vehicle, @plate, @type, @job, @stored)', {
+				['@owner'] = xPlayer.identifier,
+				['@vehicle'] = json.encode(vehicleProps),
+				['@plate'] = vehicleProps.plate,
+				['@type'] = type,
+				['@job'] = xPlayer.job.name,
+				['@stored'] = true
+			}, function (rowsChanged)
+				cb(true)
+			end)
+		else
+			cb(false)
+		end
+	end
+end)
+
+ESX.RegisterServerCallback('esx_government:storeNearbyVehicle', function(source, cb, nearbyVehicles)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local foundPlate, foundNum
+
+	for k,v in ipairs(nearbyVehicles) do
+		local result = MySQL.Sync.fetchAll('SELECT plate FROM owned_vehicles WHERE owner = @owner AND plate = @plate AND job = @job', {
+			['@owner'] = xPlayer.identifier,
+			['@plate'] = v.plate,
+			['@job'] = xPlayer.job.name
+		})
+
+		if result[1] then
+			foundPlate, foundNum = result[1].plate, k
+			break
+		end
+	end
+
+	if not foundPlate then
+		cb(false)
+	else
+		MySQL.Async.execute('UPDATE owned_vehicles SET `stored` = true WHERE owner = @owner AND plate = @plate AND job = @job', {
+			['@owner'] = xPlayer.identifier,
+			['@plate'] = foundPlate,
+			['@job'] = xPlayer.job.name
+		}, function (rowsChanged)
+			if rowsChanged == 0 then
+				print(('esx_government: %s has exploited the garage!'):format(xPlayer.identifier))
+				cb(false)
+			else
+				cb(true, foundNum)
+			end
+		end)
+	end
+end)
+
+function getPriceFromHash(vehicleHash, jobGrade, type)
+	local vehicles = Config.AuthorizedVehicles[type][jobGrade]
+
+	for k,v in ipairs(vehicles) do
+		if GetHashKey(v.model) == vehicleHash then
+			return v.price
+		end
+	end
+
+	return 0
+end
